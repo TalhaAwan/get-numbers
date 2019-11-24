442cd24a9d564d1d392aa66b1f0d3687455a3e73
TalhaAwan <talhamanzoorawan@gmail.com>
2019-11-25 00:35:29 +0500

    new lines to past linting test

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ module.exports = (text, options) => {
 	if (!text || typeof text !== 'string') {
 		return [];
 	}
+
 	numbers = text.match(/(-\d+|\d+)(,\d+)*(\.\d+)*/g);
 
 	if (options.string === false) {
 		numbers = numbers.map(n => Number(n.replace(/,/g, '')));
 	}
+
 	return numbers;
 };
